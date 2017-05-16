c48e991005ed755a0e3091b66867540eba37cff1
Shyon Chowdhury <shyonchow@gmail.com>
2017-05-16 11:23:55 +0530

    V1
    Object position correct but clipping issue persists

---
+++ Assets/PlaceMarkerlessObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Kudan.AR{
+
+	public class PlaceMarkerlessObject : MonoBehaviour {
+
+		public KudanTracker _kudantracker;
+
+		public void PlaceClick() {
+			Vector3 position;
+			Quaternion orientation;
+
+			_kudantracker.FloorPlaceGetPose(out position, out orientation);
+			_kudantracker.ArbiTrackStart(position, orientation);
+		}
+	}
+
+}
